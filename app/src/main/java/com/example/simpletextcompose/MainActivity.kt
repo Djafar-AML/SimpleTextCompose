ece91568843c1281f9a97bbd27ad5483ab32a5f6
Jeff <Jafaragha77@gmail.com>
2023-04-03 15:55:23 +0330

    - padding added to  Text

--- app/src/main/java/com/example/simpletextcompose/MainActivity.kt
+++ app/src/main/java/com/example/simpletextcompose/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.simpletextcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.simpletextcompose.ui.theme.SimpleTextComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,13 +38,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.fillMaxWidth().padding(8.dp), textAlign = TextAlign.Center)
 }
 
 @Composable
 fun GreetingButton() {
 
-    Button(onClick = {}) {
+    Button(onClick = {} ) {
         GreetingText(name = "beautiful button")
     }
 
